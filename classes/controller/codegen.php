705ce05420d2b1fe0361a9e8df1430aad094813d
jakimfett <jakimfett@gmail.com>
2015-07-28 13:36:02 -0700

    Rough initial code generation

---
+++ classes/controller/codegen.php
@@ -0,0 +1,87 @@
+<?php
+
+class Controller_Codegen extends Controller_Template
+{
+
+    public $template  = 'template/blank';
+    public $title;
+    public $limit     = 10;
+    public $locations = array('q7', '7w', 'tn');
+    public $codes     = array();
+    public $min;
+    public $max;
+
+    /**
+     * Loads the template [View] object.
+     */
+    public function before()
+    {
+        parent::before();
+
+        Theme::set_theme('labyrinth');
+
+        $this->title = 'Promo Code Generator';
+
+        $this->template->header          = View::factory('template/header');
+        $this->template->header->scripts = array(
+            'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+            'media/labyrinth/js/script.js',
+            'media/labyrinth/js/jquery.fitvids.js'
+        );
+
+        $this->template->header->title = $this->title;
+        $this->template->body          = View::factory('template/generate');
+        $this->template->footer        = View::factory('template/footer');
+
+
+        $this->limit = $this->request->param('limit', 10);
+
+        $this->min = $this->_convert_alpha_to_integer('10');
+        $this->max = $this->_convert_alpha_to_integer('zz');
+    }
+
+    public function action_generate()
+    {
+        $this->template->body->min = $this->min;
+        $this->template->body->min = $this->max;
+
+        foreach ($this->locations as $location) {
+            for ($i = 0; $i < $this->limit; $i++) {
+                $duplicate = true;
+                while ($duplicate) {
+                    $random = rand($this->min, $this->max);
+                    if (!in_array($random, $this->codes)) {
+                        $duplicate     = false;
+                        $this->codes[] = $location . $this->_convert_integer_to_alpha($random);
+                    }
+                }
+            }
+        }
+
+        $this->template->body->codes = $this->codes;
+    }
+
+    private function _convert_alpha_to_integer($alpha)
+    {
+        return base_convert($alpha, 36, 10);
+    }
+
+    private function _convert_integer_to_alpha($int)
+    {
+        return base_convert($int, 10, 36);
+    }
+
+    public function after()
+    {
+        if ($this->auto_render === TRUE) {
+            if (empty($this->template->body->content)) {
+                $this->template->body->content = '';
+            }
+            $body = $this->template->header->render();
+            $body .= $this->template->body->render();
+            $body .= $this->template->footer->render();
+            $this->response->body($body);
+        }
+    }
+
+}
