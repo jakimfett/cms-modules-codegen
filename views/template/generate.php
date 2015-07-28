60e13ede1f27c731fbed9945f630addf043bec8e
jakimfett <jakimfett@gmail.com>
2015-07-28 13:42:38 -0700

    Display stats

--- views/template/generate.php
+++ views/template/generate.php
@@ -1,4 +1,16 @@
 <?php
+echo "Min value is: " . $min;
+echo "<br/>";
+echo "Max value is: " . $max;
+echo "<br/>";
+echo "Total possible unique values: " . $total_unique;
+echo "<br/>";
+echo "Using locations: ";
+foreach ($locations as $location) {
+    echo $location . ", ";
+}
+
+echo "<hr/>";
 foreach ($codes as $code){
     echo $code . '<br/>';
 }
