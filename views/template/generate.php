0e84161ca83bba587e0f5d498ef19a5b3f475ba9
jakimfett <jakimfett@gmail.com>
2015-09-05 23:36:13 -0700

    Confirm code generation with user

--- views/template/generate.php
+++ views/template/generate.php
@@ -1,11 +1,6 @@
 <?php
-echo "Min value is: " . $min;
-echo "<br/>";
-echo "Max value is: " . $max;
-echo "<br/>";
-echo "Total possible unique values: " . $total_unique;
-
-echo "<hr/>";
-foreach ($codes as $code) {
-    echo $code . '<br/>';
+if ($success) {
+    echo "Successfully generated {$generated} promo codes";
+} else {
+    echo "Codes did not successfully generate";
 }
